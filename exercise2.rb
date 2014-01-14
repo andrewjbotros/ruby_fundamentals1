b1eed9f52fd405350e7470c3a9bded3c2bd5f9c5
andrewjbotros <andrewjbotros@gmail.com>
2014-01-14 12:09:06 -0500

    Second assignment, finished

--- exercise2.rb
+++ exercise2.rb
@@ -13,6 +13,10 @@ tip = gets.strip
 tip(bill, tip)
 
 # Try adding a string and an integer. What happens? Find a way to convert the integer so that it works and use puts to print the answer onscreen.
+puts 1.to_s + " apple a day keeps the doctor away"
 # Evidently, Ruby is much more than just a calculator, but try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+value = 45628*7839
+puts "The value is #{value}"
 # What's the value of the expression (true && false) || (false && true) || !(false && false)? Try figuring it out on your own before typing it in.
-
+print "The expression (true && false) || (false && true) || !(false && false) evaluates to " 
+puts (true && false) || (false && true) || !(false && false)
